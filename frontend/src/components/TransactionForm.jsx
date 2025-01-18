3f15fe8a32024da2e1fc84a179410bcc8cc9ae69
ss909ss <makarenkoi836@gmail.com>
2025-01-18 07:43:58 +0200

    CRUD added in client

--- frontend/src/components/TransactionForm.jsx
+++ frontend/src/components/TransactionForm.jsx
@@ -1,7 +1,17 @@
+import { useMutation } from "@apollo/client";
+import { CREATE_TRANSACTION } from "../graphql/mutations/transaction.mutation";
+import toast from "react-hot-toast";
+
 const TransactionForm = () => {
+
+	// TODO => REFETCH ONCE YOU CREATE THE TRANSACTION
+
+	const [createTransaction,{loading}] = useMutation(CREATE_TRANSACTION,{
+		refetchQueries: ["GetTransactions"],
+	});
+
 	const handleSubmit = async (e) => {
 		e.preventDefault();
-
 		const form = e.target;
 		const formData = new FormData(form);
 		const transactionData = {
@@ -12,7 +22,15 @@ const TransactionForm = () => {
 			location: formData.get("location"),
 			date: formData.get("date"),
 		};
-		console.log("transactionData", transactionData);
+		try {
+			await createTransaction({variables:{input:transactionData}})
+
+			form.reset();
+			toast.success("Transaction created successfully");
+		} catch (error) {
+			toast.error(error.message);
+		}
+		
 	};
 
 	return (
@@ -150,8 +168,9 @@ const TransactionForm = () => {
           from-pink-500 to-pink-500 hover:from-pink-600 hover:to-pink-600
 						disabled:opacity-70 disabled:cursor-not-allowed'
 				type='submit'
+				disabled={loading}
 			>
-				Add Transaction
+				{loading ? "Loading..." : "Add Transaction"}
 			</button>
 		</form>
 	);
