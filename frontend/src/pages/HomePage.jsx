8777f9d0be17963d2d5193b54b8829082427df0e
ss909ss <makarenkoi836@gmail.com>
2025-01-19 08:46:49 +0200

    fix front

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -114,7 +114,7 @@ const HomePage = () => {
 			<div className='flex flex-col gap-6 items-center max-w-7xl mx-auto z-20 relative justify-center'>
 				<div className='flex items-center'>
 					<p className='md:text-4xl text-2xl lg:text-4xl font-bold text-center relative z-50 mb-4 mr-4 bg-gradient-to-r from-pink-600 via-indigo-500 to-pink-400 inline-block text-transparent bg-clip-text'>
-						Spend wisely, track wisely
+						Spend wisely, track easily
 					</p>
 					<img
 						src={authUserData?.authUser.profilePicture}
