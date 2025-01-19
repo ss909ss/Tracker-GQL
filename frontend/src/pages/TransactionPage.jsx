886d9055870004a5033103404c3dfd16fbc41272
ss909ss <makarenkoi836@gmail.com>
2025-01-19 08:40:02 +0200

    fix all problems

--- frontend/src/pages/TransactionPage.jsx
+++ frontend/src/pages/TransactionPage.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import {useParams} from 'react-router-dom';
 import TransactionFormSkeleton from "../components/skeletons/TransactionFormSkeleton";
 import { useMutation, useQuery } from "@apollo/client";
-import { GET_TRANSACTION } from "../graphql/queries/transaction.query";
+import { GET_TRANSACTION, GET_TRANSACTION_STATISTICS } from "../graphql/queries/transaction.query";
 import { UPDATE_TRANSACTION } from "../graphql/mutations/transaction.mutation";
 import toast from "react-hot-toast";
 
@@ -15,7 +15,7 @@ const TransactionPage = () => {
 	console.log("Transaction",data);
 
 	const [updateTransaction,{loading:loadingUpdate}] = useMutation(UPDATE_TRANSACTION,{
-		refetchQueries: [{query:GetTransactionStatistics}],
+		refetchQueries: [{query:GET_TRANSACTION_STATISTICS}],
 	});
 
 	const [formData, setFormData] = useState({
